d928215c754cfbfe4565577cf3001a908d7eec28
anneu <b.imnuj@gmail.com>
2023-10-13 18:16:43 +0900

    setup pre-onboarding project

---
+++ src/main/java/wanted/preonboarding/PreOnboardingApplication.java
@@ -0,0 +1,13 @@
+package wanted.preonboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreOnboardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreOnboardingApplication.class, args);
+	}
+
+}
