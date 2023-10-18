45c04927713fed8782e58c26f7c25321d32769f8
anneu <b.imnuj@gmail.com>
2023-10-18 23:49:08 +0900

    feat: DB에 회사 샘플 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
 insert into company (name, country, region) values ('원티드랩', '한국', '서울');
+insert into company (name, country, region) values ('사람인', '한국', '서울');
 
 insert into member (name, age) values ('짱구', '20');
 insert into member (name, age) values ('철수', '20');
